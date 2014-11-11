4318381512ec0ce8fba062461a8912fe6302eba0
Pavel Mayer <pavel@hoccer.com>
2014-11-11 02:54:53 +0100

    - DELETE requires write permissions for the file now

--- Extensions/WebDAV/DELETEResponse.m
+++ Extensions/WebDAV/DELETEResponse.m
@@ -9,17 +9,60 @@ static const int httpLogLevel = HTTP_LOG_LEVEL_WARN;
 
 @implementation DELETEResponse
 
-- (id) initWithFilePath:(NSString*)path {
-  if ((self = [super init])) {
-    BOOL exists = [[NSFileManager defaultManager] fileExistsAtPath:path];
-    if ([[NSFileManager defaultManager] removeItemAtPath:path error:NULL]) {
-      _status = exists ? 200 : 204;
-    } else {
-      HTTPLogError(@"Failed deleting \"%@\"", path);
-      _status = 404;
++ (short)getPosixPermissionsForPath:(NSString*)myFilePath {
+    NSError * error = nil;
+    NSDictionary * attributes = [[NSFileManager defaultManager] attributesOfItemAtPath:myFilePath error:&error];
+    if (error != nil) {
+        NSLog(@"Error setting posix permission for path %@, error=%@", myFilePath, error);
+        return -1;
     }
-  }
-  return self;
+    return [attributes filePosixPermissions];
+}
+
+/*
++ (BOOL)setPosixPermissionsReadOnlyForPath:(NSString*)myFilePath {
+    return [self setPosixPermissions:[@(0444) shortValue] forPath:myFilePath];
+}
+
++ (BOOL)setPosixPermissionsReadWriteForPath:(NSString*)myFilePath {
+    return [self setPosixPermissions:[@(0644) shortValue] forPath:myFilePath];
+}
+*/
+
++(BOOL)isUserReadOnlyFile:(NSString*)myFilePath {
+    short permissions = [self getPosixPermissionsForPath:myFilePath];
+    if (permissions != -1) {
+        return (permissions & 0200) == 0;
+    }
+    return NO;
+}
+/*
++(BOOL)isUserReadWriteFile:(NSString*)myFilePath {
+    short permissions = [self getPosixPermissionsForPath:myFilePath];
+    if (permissions != -1) {
+        return (permissions & 0600) == 0600;
+    }
+    return NO;
+}
+*/
+
+- (id) initWithFilePath:(NSString*)path {
+    if ((self = [super init])) {
+        BOOL exists = [[NSFileManager defaultManager] fileExistsAtPath:path];
+        BOOL readOnly = [DELETEResponse isUserReadOnlyFile:path];
+        if (readOnly) {
+            _status = 405;
+            HTTPLogError(@"Failed deleting readonly file \"%@\"", path);
+        } else {
+            if ([[NSFileManager defaultManager] removeItemAtPath:path error:NULL]) {
+                _status = exists ? 200 : 204;
+            } else {
+                HTTPLogError(@"Failed deleting \"%@\"", path);
+                _status = 404;
+            }
+        }
+    }
+    return self;
 }
 
 - (UInt64) contentLength {
